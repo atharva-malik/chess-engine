a68c436de98805b5dcf48f98e5104d180f50b027
Atharva <atharva.malik@gmail.com>
2025-02-25 22:43:06 +1100

    started work on some sort of eval.
    Hopefully won't lose my mind like last time

--- Initial Experiments/interface.py
+++ Initial Experiments/interface.py
@@ -4,11 +4,14 @@
     Keep it nice and simple
 """
 from minimax_bot import Bot
+import chess
 
-bot = Bot()
-while bot.gameStage == "O":
-    bot.board.push_san(bot.getMove())
-    print(bot)
-    print(bot.gameStage)
-    print(bot.getFenForOpening())
-    print()
+bot = Bot(chess.Board(fen="8/7p/5kp1/p1b1r3/P1P5/1P3B1P/4p2K/4B3 w - - 2 45"))
+# while bot.gameStage == "O":
+#     bot.board.push_san(bot.getMove())
+#     print(bot)
+#     print(bot.gameStage)
+#     print(bot.getFenForOpening())
+#     print()
+print(bot)
+print(bot.minimax(10, -9999, 9999, False, bot.board))
--- Initial Experiments/minimax_bot.py
+++ Initial Experiments/minimax_bot.py
@@ -1,8 +1,9 @@
 import chess, json, random
 
 class Bot:
-    def __init__(self, board=chess.Board(), turn=0):
+    def __init__(self, board=chess.Board(), colour="w", turn=0):
         self.board = board #* Initialise the board
+        self.colour = colour #* Set the colour, w for white, b for black
         self.turn = turn #* Set the turn, 0 for white, 1 for black
         self.gameStage = "O" #* Set the game stage, O for opening, M for middle game, E for end game
     
@@ -30,6 +31,72 @@ class Bot:
 
     def getFenForOpening(self):
         return self.board.fen()[0:-4]
+
+    def minimax(self, depth, alpha, beta, isMaximizing, board=chess.Board()):
+        if depth == 0:
+            return self.evaluateMiddleGame()
+        elif board.is_checkmate():
+            return None
+        
+        if isMaximizing:
+            maxEval = -9999
+            for move in board.legal_moves:
+                board.push(move)
+                evaluation = self.minimax(depth - 1, alpha, beta, False, board)
+                board.pop()
+                maxEval = max(maxEval, evaluation)
+                alpha = max(alpha, evaluation)
+                if beta <= alpha:
+                    break
+            return maxEval
+        else:
+            minEval = 9999
+            for move in board.legal_moves:
+                board.push(move)
+                evaluation = self.minimax(depth - 1, alpha, beta, True, board)
+                board.pop()
+                minEval = min(minEval, evaluation)
+                beta = min(beta, evaluation)
+                if beta <= alpha:
+                    break
+            return minEval
+        
+    
+    def evaluateMiddleGame(self, perspective=1):
+        """The evaluation function for the middle game
+
+        Args:
+            perspective (int, optional): A positive would mean from white's perspective, negative would mean from. Defaults to 1.
+
+        Returns:
+            float: the evaluation of the position
+        """
+        #? Going to start with a simple evaluation where it values piece value over anything else
+        pieces = self.getNumberOfPieces()
+        whiteEval = 0
+        blackEval = 0
+        
+        whiteEval += pieces["P"] * 1
+        whiteEval += pieces["N"] * 3
+        whiteEval += pieces["B"] * 3
+        whiteEval += pieces["R"] * 5
+        whiteEval += pieces["Q"] * 9
+        
+        blackEval += pieces["p"] * 1
+        blackEval += pieces["n"] * 3
+        blackEval += pieces["b"] * 3
+        blackEval += pieces["r"] * 5
+        blackEval += pieces["q"] * 9
+        
+        return (whiteEval - blackEval) * perspective
+
+    def getNumberOfPieces(self):
+        pieces = {"r": 0, "n": 0, "b": 0, "q": 0, "k": 0, "p": 0, "R": 0, "N": 0, "B": 0, "Q": 0, "K": 0, "P": 0}
+        board = str(self.board)
+        for i in board:
+            if i in pieces.keys():
+                pieces[i] += 1
+        return pieces
 
     def __str__(self):
         return str(self.board)
