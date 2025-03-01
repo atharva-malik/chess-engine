c5464a79746a1ab05adb97d8e73bc939a60937e5
Atharva <atharva.malik@gmail.com>
2025-03-01 18:03:29 +1100

    implemented the piece square values
    going to try multi-threading/multi-processing as it is running way too slow

--- Initial Experiments/interface.py
+++ Initial Experiments/interface.py
@@ -6,8 +6,8 @@
 from minimax_bot import Bot
 import chess
 
-# bot = Bot(chess.Board(fen="8/7p/5kp1/p1b1r3/P1P5/1P3B1P/4p2K/4B3 w - - 2 45"))
-bot = Bot()
+bot = Bot(chess.Board(fen="8/7p/5kp1/p1b1r3/P1P5/1P3B1P/4p2K/4B3 w - - 2 45"))
+# bot = Bot()
 # while bot.gameStage == "O":
 #     bot.board.push_san(bot.getMove())
 #     print(bot)
@@ -17,7 +17,7 @@ bot = Bot()
 turn = 0
 while True:
     print(str(bot) + "\n")
-    print(bot.evaluateMiddleGame(bot.board))
+    print(bot.evaluateThePosition(bot.board))
     if turn == 0:
         bot.board.push_san(bot.getBestMove(5, bot.board, "w"))
         turn = 1
